9d8c95621fe9f3e2a55fd944d7f99ad3df0abbf9
himabh4 <kimadipa@gmail.com>
2016-03-30 10:54:19 +0700

    Update Fibonaci.java

--- Fibonaci.java
+++ Fibonaci.java
@@ -5,14 +5,18 @@ public class Fibonaci {
 
   
     public static void main(String[] args) {
-        System.out.print("Masukan angka : ");
+       System.out.print("Masukan angka : ");
         int inp = new Scanner(System.in).nextInt();
         int total=0;
+        int jumlah[] = new int[100];
         System.out.println("");
         System.out.println("");
         System.out.println("Hasil : ");
         for(int i=1;i<=inp;i++){
-            System.out.print(fibonacciRecusion(i)+" ");
+            jumlah[i]=fibonacciRecusion(i);
+        }
+        for(int i=inp;i>=1;i--){
+            System.out.print(jumlah[i]+" ");
         }
     }
     public static int fibonacciRecusion(int number){
